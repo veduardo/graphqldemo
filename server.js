622141477a3763d45e228888651b214aa7460b48
Pedro Siqueira <pedrowss@gmail.com>
2017-03-09 16:18:05 -0300

    Add schema file

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema.js');
 
 const app = express();
 
 app.use('/graphql', expressGraphQL({
+  schema,
   graphiql: true
 }));
 
