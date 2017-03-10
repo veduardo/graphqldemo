8cdc96a0c0d114f4fa28334e6b2be7b0a9a978e5
Pedro Siqueira <pedrowss@gmail.com>
2017-03-10 17:58:22 -0300

    Add delete user mutation

--- schema/schema.js
+++ schema/schema.js
@@ -73,8 +73,18 @@ const mutation = new GraphQLObjectType({
         age: { type: new GraphQLNonNull(GraphQLInt) },
         companyId: { type: GraphQLString }
       },
-      resolve(parentValue, { firstName, age }) {
-        return axios.post('http://localhost:3000/users', { firstName, age })
+      resolve(parentValue, { firstName, age, companyId }) {
+        return axios.post('http://localhost:3000/users', { firstName, age, companyId })
+          .then(response => response.data);
+      }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, { id }) {
+        return axios.delete(`http://localhost:3000/users/${id}`)
           .then(response => response.data);
       }
     }
